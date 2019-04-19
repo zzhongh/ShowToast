dc5d7ff6b7f12a3ad8fc59a60ede86e84ae68b32
DESKTOP-DNS4SG5\Administrator <735346269@qq.com>
2019-04-19 13:33:50 +0800

    commit
    commit

---
+++ mylibrary/src/main/java/com/gyb/zp/mylibrary/Test.java
@@ -0,0 +1,14 @@
+package com.gyb.zp.mylibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by Administrator on 2019/4/19.
+ */
+
+public class Test {
+    public static void Test(Context context) {
+        Toast.makeText(context,"cccccccccccccc",Toast.LENGTH_SHORT).show();
+    }
+}
